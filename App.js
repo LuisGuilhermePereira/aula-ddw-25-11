554be47d1ab27707b53d296b86c581534040694e
LuisGuilhermePereira <luisguilhermep886@gmail.com>
2021-12-02 17:01:05 +0000

    aula ddw 02/12

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,TextInput, TouchableOpacity } from 'react-native';
 
 function enviar(){
   alert('Você clicou no botão!');
@@ -9,7 +9,7 @@ function enviar(){
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.titulo}>Segundo Aplicativo</Text>
+      <Text style={styles.titulo}>TRICAMPEÃO</Text>
       <TextInput style={styles.campo}></TextInput>
       <TouchableOpacity style={styles.botao} onPress={enviar}>
         <Text style={styles.textobotao}>Mostrar</Text>
